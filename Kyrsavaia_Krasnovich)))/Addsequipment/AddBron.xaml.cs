58b6e471efb8701ea57d46e26d3ac4b057e58b0d
Alexunder <email@example.com>
2022-03-28 12:48:04 +0300

    Добавьте файлы проекта.

---
+++ Kyrsavaia_Krasnovich)))/Addsequipment/AddBron.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kyrsavaia_Krasnovich___
+{
+    /// <summary>
+    /// Логика взаимодействия для AddBron.xaml
+    /// </summary>
+    public partial class AddBron : Page
+    {
+        public AddBron()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Kyrsavaia_Krasnovich)))/OsnovaMenuUser.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kyrsavaia_Krasnovich___
+{
+    /// <summary>
+    /// Логика взаимодействия для OsnovaMenuUser.xaml
+    /// </summary>
+    public partial class OsnovaMenuUser : Window
+    {
+        public OsnovaMenuUser()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
